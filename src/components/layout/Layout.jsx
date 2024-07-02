193e6c0dd41224dc83cf0c3651bdacb5167f37f2
あかり <>
2024-07-02 12:10:11 +0900

    Hourly display

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,8 +2,11 @@ import { Grid, Box, Container, CssBaseline } from "@mui/material";
 import Search from "../Search";
 import Main from "../Main";
 import { useState, useEffect } from "react";
+import { iconMapping, iconDefaults } from '../../constants'
+import ReactAnimatedWeather from 'react-animated-weather';
+import { DateTime } from 'luxon'
 
-const Layout = ({ region, setRegion, fetchWeather, currentWeather, searchedLocationWeather }) => {
+const Layout = ({ region, setRegion, fetchWeather, currentWeather, currentForecast, searchedLocationWeather, searchedLocationForecast, mode }) => {
 
   const [bgImage, setBgImage] = useState("black")
 
@@ -30,20 +33,32 @@ const Layout = ({ region, setRegion, fetchWeather, currentWeather, searchedLocat
         <Search region={region} setRegion={setRegion} fetchWeather={fetchWeather} />
         <Box bgcolor="transparent" p={2} flexGrow={1} color="white">
           <Box height="25vh" mb={24} textAlign='center'>
-            <Main currentWeather={currentWeather} searchedLocationWeather={searchedLocationWeather} />
+            <Main currentWeather={currentWeather} searchedLocationWeather={searchedLocationWeather} mode={mode} />
           </Box>
 
           <Grid container spacing={4} textAlign='center'>
-            <Grid item xs={4}>
-              <Box sx={{ height: 240, padding: 2, backgroundColor: "rgba(255,255,255,0.2)", color: 'white' }}>
-                hourly
-              </Box>
-            </Grid>
-            <Grid item xs={8}>
-              <Box sx={{ height: 240, padding: 2, backgroundColor: "rgba(255,255,255,0.2)", color: 'white' }}>
-                週間天気
-              </Box>
-            </Grid>
+            {
+              searchedLocationForecast.map(forecast =>
+                <Grid item key={forecast.id} xs={4}>
+                  <Box sx={{ height: 240, padding: 2, backgroundColor: "rgba(255,255,255,0.2)", color: 'white' }}>
+
+                    <ReactAnimatedWeather
+                      icon={iconMapping[forecast.icon] ?? 'CLEAR_DAY'}
+                      color={iconDefaults.color}
+                      size={30}
+                      animate={iconDefaults.animate}
+                    /><br />
+                    {forecast.main}<br />
+                    {forecast.max_temp}℃
+                    <br />
+                    {
+                      DateTime.fromISO(forecast.forecast_time).toRelative({ unit: 'hour' })
+                    }
+                  </Box>
+                </Grid>
+
+              )
+            }
           </Grid>
 
         </Box>
